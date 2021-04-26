90c7e60adb50a5e087b044e424f80ca913f89119
gaozhichang <gaozhichang@qq.com>
2021-04-26 16:48:25 +0800

    Update 1011.py

--- 1011.py
+++ 1011.py
@@ -8,6 +8,9 @@
 来源：力扣（LeetCode）
 链接：https://leetcode-cn.com/problems/capacity-to-ship-packages-within-d-days
 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+8%
+9%
 '''
 import math 
 def shipWithinDays(weights, D):
