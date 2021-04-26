8db18ede491385d65ba1e75102d7bcc648c276fc
gaozhichang <gaozhichang@qq.com>
2021-04-26 15:40:34 +0800

    Update 1011.py

--- 1011.py
+++ 1011.py
@@ -1,3 +1,14 @@
+/*
+传送带上的包裹必须在 D 天内从一个港口运送到另一个港口。
+
+传送带上的第 i 个包裹的重量为 weights[i]。每一天，我们都会按给出重量的顺序往传送带上装载包裹。我们装载的重量不会超过船的最大运载重量。
+
+返回能在 D 天内将传送带上的所有包裹送达的船的最低运载能力。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/capacity-to-ship-packages-within-d-days
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
 import math 
 def shipWithinDays(weights, D):
         """
